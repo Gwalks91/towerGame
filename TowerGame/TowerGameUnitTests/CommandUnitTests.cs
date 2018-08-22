4a415b75b91a75716c9d22654eac1d918fe24e74
Grant Walker <gwwalker91@gmail.com>
2018-08-21 22:33:01 -0700

    I added the Testing project to the solution.

---
+++ TowerGame/TowerGameUnitTests/CommandUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommandUnitTest
+{
+    [TestClass]
+    public class CommandUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("test", "test");
+        }
+    }
+}
---
+++ TowerGame/TowerGameUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85fe472c-3f00-4e0a-ab1a-4da2c8ccb940")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
